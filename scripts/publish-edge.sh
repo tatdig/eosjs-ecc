9a5d14e0bc4764688d47ec84076e0b2cfd3e234c
Nelson Ramirez <nelson.ramirez@block.one>
2019-07-03 14:15:44 -0400

    make scripts executable

--- scripts/publish-edge.sh
+++ scripts/publish-edge.sh
@@ -4,27 +4,27 @@ echo "Starting publish edge"
 
 npm doctor
 
-# . "${TRAVIS_BUILD_DIR}/scripts/publish-utils.sh";
+. "${TRAVIS_BUILD_DIR}/scripts/publish-utils.sh";
 
-# echo "Running on branch/tag ${TRAVIS_BRANCH}":
+echo "Running on branch/tag ${TRAVIS_BRANCH}":
 
-# echo "Setting up git"
-# setup_git
+echo "Setting up git"
+setup_git
 
-# echo "Creating new version"
-# git checkout -- .
+echo "Creating new version"
+git checkout -- .
 
-# git status 
+git status 
 
-# # get the short commit hash to include in the npm package
-# current_commit="$(git rev-parse --short HEAD)";
+# get the short commit hash to include in the npm package
+current_commit="$(git rev-parse --short HEAD)";
 
-# npm version prerelease -preid "${current_commit}" -no-git-tag-version
+npm version prerelease -preid "${current_commit}" -no-git-tag-version
 
-# git commit -a -m "Updating version [skip ci]"
+git commit -a -m "Updating version [skip ci]"
 
-# echo "Publish to NPM"
+echo "Publish to NPM"
 
-# cp .npmrc.template $HOME/.npmrc 
+cp .npmrc.template $HOME/.npmrc 
 
-# npm publish --tag edge
+npm publish --tag edge
