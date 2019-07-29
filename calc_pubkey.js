e0410f006799076673e7b6b35d947267635ff1fb
tatdig <32492514+tatdig@users.noreply.github.com>
2019-07-29 19:59:56 -0400

    Use example detailed

--- calc_pubkey.js
+++ calc_pubkey.js
@@ -1,14 +1,24 @@
 #!/usr/bin/node
 // EXAMPLE: node calc_pubkey.js 4cZLxRNk6ZLuzH7ZCPdfez7HN7YFsTPPjLK5LDEZ5SVmnSnTsWT
+// ./calc_pubkey.js 4cZLxRNk6ZLuzH7ZCPdfez7HN7YFsTPPjLK5LDEZ5SVmnSnTsWT
+//WIF:  4cZLxRNk6ZLuzH7ZCPdfez7HN7YFsTPPjLK5LDEZ5SVmnSnTsWT
+//Private Key:  cdecd13cc81ff00566a34bb37758befacecdbaabbe86141e4a1804a2af49e60e
+//Public key (compressed):  03d3f28b43c6ef8c625d23cf3b8c37167e9ca31b16ed1e097aad1a5a8d9a37e2f5
+//Public key:  04d3f28b43c6ef8c625d23cf3b8c37167e9ca31b16ed1e097aad1a5a8d9a37e2f5c20e83ba5615eea11a762e71550cee7e5c0fca9e212578392e647416a3d9bd1f
+//Binary address (compressed):  419edb048a4600553e5f04b2753219636f6af5612598a747b7
+//Binary address:  41fed09045a339356220519e03c8888570b25bed6e9e32d9d1
+//TDCoin address (compressed):  TQTA8zDDrCK36WasNnRZdFaWBz8zzhJwGr
+//TDCoin address:  TZCYV2wec9c5TsUDiged8qFSHKgRfR54PA
+
 
 const ecc = require('eosjs-ecc');
 const base58 = require('bs58');
 const ripemd160 = require('ripemd160')
 
-//const wif = process.argv[2];
-//console.log("WIF: ", wif);
+const wif = process.argv[2];
+console.log("WIF: ", wif);
 
-const privkey = ecc.PrivateKey.fromString(process.argv[2]);
+const privkey = ecc.PrivateKey.fromString(wif);
 console.log("Private Key: ", privkey.toBuffer().toString('hex'));
 
 const compressed_pubkey = privkey.toPublic();
